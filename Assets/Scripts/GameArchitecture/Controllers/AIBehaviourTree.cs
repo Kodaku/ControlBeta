406bda2e621f396af17229cb630e115cc4422979
Axel_Mastroianni <a.mastroianni@studenti.unibs.it>
2021-10-29 15:01:29 +0200

    Added the Main Menu Next Step: Pause Menu

--- Assets/Scripts/GameArchitecture/Controllers/AIBehaviourTree.cs
+++ Assets/Scripts/GameArchitecture/Controllers/AIBehaviourTree.cs
@@ -8,12 +8,15 @@ public class AIBehaviourTree : MonoBehaviour
 {
     [SerializeField] private float startDistanceRadius = 10.0f;
     [SerializeField] private float distanceRadius = 20.0f;
+    [SerializeField] private GameObject chargingAura;
     private Vector3 playerPosition;
     // private AIPlayerMovement aIPlayerMovement;
     private AIPlayerAnimations aIPlayerAnimations;
     private NavMeshAgent agent;
     private Perceivable<Vector3> positionSensor;
     private Perceivable<float>[] updatableSensors = new Perceivable<float>[2];
+    private bool animationCompleted = false;
+    private float gap = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +26,13 @@ public class AIBehaviourTree : MonoBehaviour
         positionSensor = GetComponentInChildren<PositionSensor>();
         updatableSensors[(int)UpdatableIndices.HEALTH] = GetComponentInChildren<HealthSensor>();
         updatableSensors[(int)UpdatableIndices.MANA] = GetComponentInChildren<ManaSensor>();
+
+        chargingAura.gameObject.SetActive(false);
+    }
+
+    public void SetAnimationCompleted()
+    {
+        this.animationCompleted = true;
     }
 
     //WANDER SECTION
@@ -70,5 +80,71 @@ public class AIBehaviourTree : MonoBehaviour
         }
     }
 
-    //TODO: FLEE SECTION
+    //FLEE SECTION
+    [Task]
+    public bool HealthLessThan(int dangerValue)
+    {
+        float currentHealth = updatableSensors[(int)UpdatableIndices.HEALTH].GetMeasure();
+        return currentHealth < dangerValue;
+    }
+
+    [Task]
+    public bool InDanger(int minDistance)
+    {
+        playerPosition = positionSensor.GetMeasure();
+        Vector3 distance = playerPosition - this.transform.position;
+        return (distance.magnitude < minDistance);
+    }
+
+    [Task]
+    public void Escape()
+    {
+        if(Task.current.isStarting)
+        {
+            aIPlayerAnimations.Evade();
+        }
+
+        if(animationCompleted)
+        {
+            animationCompleted = false;
+            Task.current.Succeed();
+        }
+    }
+
+    [Task]
+    public void PickFleeDestination()
+    {
+        float currentX = this.transform.position.x;
+        float currentRadius = Vector3.Distance(this.transform.position, playerPosition);
+        float destinationX = currentX - startDistanceRadius + currentRadius - Random.Range(0, gap);
+        float destinationZ = this.transform.position.z;
+        Vector3 fleeDest = new Vector3(destinationX, this.transform.position.y, destinationZ);
+        agent.SetDestination(fleeDest);
+        Task.current.Succeed();
+    }
+
+    // CHARGE SECTION
+
+    [Task]
+    public void Charge()
+    {
+        float currentMana = updatableSensors[(int)UpdatableIndices.MANA].GetMeasure();
+        if(Task.current.isStarting)
+        {
+            chargingAura.gameObject.SetActive(true);
+        }
+        aIPlayerAnimations.ChargingEnergy(true);
+        if(currentMana > 750)
+        {
+            Task.current.Succeed();
+        }
+    }
+
+    [Task]
+    public void EndCharge()
+    {
+        aIPlayerAnimations.ChargingEnergy(false);
+        chargingAura.gameObject.SetActive(false);
+        Task.current.Succeed();
+    }
 }
